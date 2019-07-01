051bbb8d8debb6c9a3278fdaabcfebcf2c4983f0
Ross Greene <Ross.Greene@softwire.com>
2019-07-01 10:49:53 +0100

    one done

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,7 @@
 import Piece from './piece';
+import Square from '../../../src/engine/square';
+import Player from '../../../src/engine/player';
+
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +9,18 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let possibleMoves = new Array(0);
+        let mySquare = board.findPiece(this);
+        let myRow = mySquare.row;
+        let myColumn = mySquare.col;
+        console.log(this.player);
+
+        if (this.player === Player.WHITE){
+            possibleMoves.push(new Square(myRow + 1, myColumn));
+        } else if (this.player === Player.BLACK){
+            possibleMoves.push(new Square(myRow - 1, myColumn ));
+        }
+        console.log('Possible Moves: ' + possibleMoves);
+        return possibleMoves
     }
 }
