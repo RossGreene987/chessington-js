091fdff9ab4f1cc1af9f1ce6425fcb7bcf582154
Ross Greene <Ross.Greene@softwire.com>
2019-07-01 11:20:10 +0100

    3 done

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -12,5 +12,4 @@ export default class Piece {
         board.movePiece(currentSquare, newSquare);
     }
 
-
 }
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from "../player";
+import Square from '../../../src/engine/square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,34 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let mySquare = board.findPiece(this);
+        let myRow = mySquare.row;
+        let myColumn = mySquare.col;
+
+        let colMoves = (this.getColumnMoves(myRow, myColumn));
+        let rowMoves = (this.getRowMoves(myRow, myColumn));
+
+        return rowMoves.concat(colMoves)
     }
+
+    getColumnMoves(row, column){
+        let colMoves = new Array(0);
+        for(let i=0; i<8; i++){
+            if (!(i===column)){
+                colMoves.push(new Square(row, i))
+            }
+        }
+        return colMoves
+    }
+
+    getRowMoves(row, column){
+        let rowMoves = new Array(0);
+        for(let i=0; i<8; i++){
+            if (!(i===row)){
+                rowMoves.push(new Square(i, column))
+            }
+        }
+        return rowMoves
+    }
+
 }
